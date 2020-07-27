5813c0c1951964ba24489827f6803411913ed4ea
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-27 16:49:22 -0700

    extract hitters & pitchers' endpoints by team

--- webscrape.py
+++ webscrape.py
@@ -1,8 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
+YEAR = 2020
+
 # define team class
 class Team:
+    def __init__(self, name, endpoint):
+        """
+        instantiate team class
+        batting: list of batting Player objects
+        pitching: list of pitching Player objects
+        :param name: name of team
+        :param endpoint: endpoint of team on website
+        """
+        self.name = name
+        self.endpoint = endpoint
+        self.batting = []
+        self.pitching = []
+# define Member class
+class Member:
     def __init__(self, name, endpoint):
         self.name = name
         self.endpoint = endpoint
@@ -22,4 +38,17 @@ for result in results:
     teams.append(Team(result.string,result['href']))
 
 # get roster for each team
+for team in teams:
+    print(team.name)
+    page = requests.get(URL + team.endpoint + str(YEAR) + ".shtml")
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # get batting roster
+    results = soup.select("#team_batting tbody tr a")
+    for result in results:
+        team.batting.append(Member(result.string, result['href']))
+    # get pitching roster
+    results = soup.select("#team_pitching tbody tr a")
+    for result in results:
+        team.pitching.append(Member(result.string, result['href']))
 
+#
