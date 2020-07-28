f6c68f3756987706a4566dff8551726a01297503
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-28 16:09:46 -0700

    implemented extractFromTable(), searchForTable() for modularity

--- code/webscrape.py
+++ code/webscrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from bs4 import Comment
 import code.Player as Player
 
 URL = 'https://www.baseball-reference.com'
@@ -58,6 +59,79 @@ def extractRoster(teams):
         for result in results:
             team.pitching.append(Member(result.string, result['href']))
 
+def searchForTable(soup, id):
+    """
+    searches for the table given the id of the table
+    :param soup: soup object containing the page
+    :param id: id of the table
+    :return: soup of table if found, None if not found
+    """
+    table = soup.find(id=id)
+    # in case data is commented out
+    if table == None:
+        comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+        id = "id=\"" + id + "\""
+        for comment in comments:
+            if id in comment:
+                table = comment
+                table = table[len("<!--"):-len("-->")]
+                table = BeautifulSoup(table, 'html.parser')
+                return table
+        # data not found even in comments:
+        return None
+    else:
+        return table
+
+def extractFromTable(table: BeautifulSoup, type: str):
+    """
+    extracts data from table depending on type
+    :param table: soup object of table
+    :param type: string that specifies hitter or pitcher
+    :return: dictionary of data extracted
+    """
+    data = {}
+    if type == "hitter":
+        # TODO: identify all necessary data
+        # in case table not found
+        plate_appearance = 0
+        hits = 0
+        # in case table found
+        if table != None:
+            a = table.find("a", string="162 Game Avg.")
+            th = a.find_parent("th")
+            plate_appearance = int(th.find_next_sibling(attrs={"data-stat": "PA"}).string)
+            hits = int(th.find_next_sibling(attrs={"data-stat": "H"}).string)
+        # calculate all stats
+        if plate_appearance == 0:
+            data['rhp'] = 0
+        else:
+            data['rhp'] = hits / plate_appearance
+        data['lhp'] = 1
+        data['pow'] = 2
+        data['avg'] = 3
+        data['fin'] = 4
+        data['gro'] = 5
+        data['fly'] = 6
+        data['hme'] = 7
+        data['awy'] = 8
+        return data
+    elif type == "pitcher":
+        # TODO: identify all necessary data
+        # in case search not found
+        innings_pitched = 0
+        hits = 0
+        # in case search found
+        if table != None:
+            a = table.find("a", string="162 Game Avg.")
+            th = a.find_parent("th")
+            innings_pitched = float(th.find_next_sibling(attrs={"data-stat": "IP"}).string)
+            hits = int(th.find_next_sibling(attrs={"data-stat": "H"}).string)
+        # calculate stats
+        data['rhb'] = hits / innings_pitched
+        data['lhb'] = 1
+        return data
+    else:
+        raise ValueError("argument 'type' must either be 'hitter' or 'pitcher'")
 def extractData(teams):
     """
     extracts necessary data from website to instantiate Player objects
@@ -73,38 +147,24 @@ def extractData(teams):
             # extract necessary data
             page = requests.get(URL + member.endpoint)
             soup = BeautifulSoup(page.content, 'html.parser')
-            table = soup.find(id="batting_standard")
-            a = table.find("a", string="162 Game Avg.")
-            th = a.find_parent("th")
-            # TODO: identify all necessary data
-            plate_appearance = th.find_next_sibling(attrs={"data-stat": "PA"})
-            hits = th.find_next_sibling(attrs={"data-stat": "H"})
-            rhp = int(hits.string) / int(plate_appearance.string)
-            lhp = 1
-            pow = 2
-            avg = 3
-            fin = 4
-            gro = 5
-            fly = 6
-            hme = 7
-            awy = 8
+            # search for table
+            table = searchForTable(soup, "batting_standard")
+            data = extractFromTable(table, "hitter")
             # instantiate hitter & add to list
-            hitter = Player.Hitter(member.name,rhp,lhp,pow,avg,fin,gro,fly,hme,awy)
+            hitter = Player.Hitter(member.name,data['rhp'],data['lhp'],
+                                   data['pow'],data['avg'],data['fin'],
+                                   data['gro'],data['fly'],data['hme'],data['awy'])
             hitters.append(hitter)
         for member in team.pitching:
             print(member.name)
             page = requests.get(URL + member.endpoint)
             soup = BeautifulSoup(page.content, 'html.parser')
-            table = soup.find(id="pitching_standard")
-            a = table.find("a", string="162 Game Avg.")
-            th = a.find_parent("th")
-            # TODO: identify all necessary data
-            innings_pitched = th.find_next_sibling(attrs={"data-stat": "IP"})
-            hits = th.find_next_sibling(attrs={"data-stat": "H"})
-            rhb = int(hits.string) / float(innings_pitched.string)
-            lhb = 1
+            # search for table
+            table = searchForTable(soup, "pitching_standard")
+            # extract data
+            data = extractFromTable(table, "pitcher")
             # instantiate pitcher & add to list
-            pitcher = Player.Pitcher(member.name, rhb, lhb)
+            pitcher = Player.Pitcher(member.name, data['rhb'], data['lhb'])
             pitchers.append(pitcher)
     return hitters, pitchers
 
