65bae56718e5059d938d126d87abf9ee96e9d2ba
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-28 00:11:16 -0700

    fixed & tested extractTeams()

--- test/test_webscrape.py
+++ test/test_webscrape.py
@@ -1,4 +1,11 @@
 import unittest
 import webscrape
 
-class
+class TestWebscrape(unittest.TestCase):
+    def test_extractTeams_extractsAllActiveTeams(self):
+        teams = webscrape.extractTeams()
+
+        self.assertEqual(len(teams),30)
+
+if __name__ == "__main__":
+    unittest.main()
