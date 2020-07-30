2ef4d678ea6cb0211a8332b188ead31c1d56e301
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-30 15:26:03 -0700

    basic structure for Game object

---
+++ code/Game.py
@@ -0,0 +1,24 @@
+import code.Player as Player
+
+class Game:
+    def __init__(self, id, hitters, pitchers, outcome):
+        """
+        instantiates Game object
+        :param id: id of the game (endpoint_game + '-A' or '-H' for away or home)
+        :param hitters: list of Hitter objects
+        :param pitchers: list of Pitcher objects
+        :param outcome: dictionary that maps hitter's name to percentage
+         the player contributed to team's hits
+        """
+        self.id = id
+        self.hitters = hitters
+        self.pitchers = pitchers
+        self.outcome = outcome
+
+    def convertToNDArray(self):
+        """
+        converts the object into ND array for model input
+        so that the model can actually do calculations with it
+        :return: X, y
+        """
+        pass
