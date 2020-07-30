d9ebfee4bf626b3f69dc918f8ee9c7345e729f2f
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-07-30 13:40:43 -0700

    renamed ws to wst

--- test/test_webscrape_training.py
+++ test/test_webscrape_training.py
@@ -1,16 +1,16 @@
 import unittest
-from code import webscrape_training as ws
+from code import webscrape_training as wst
 
 class TestWebscrape(unittest.TestCase):
     def test_extractGamesFromSeason_extractsAllEndpoints(self):
-        endpoints_game = ws.extractGamesFromSeason(2015)
+        endpoints_game = wst.extractGamesFromSeason(2015)
 
         self.assertEqual(2465,len(endpoints_game))
 
     def test_extractTeams_extractsTeams(self):
         endpoint_game = "/boxes/ARI/ARI201504060.shtml"
 
-        team_away, team_home = ws.extractTeams(endpoint_game)
+        team_away, team_home = wst.extractTeams(endpoint_game)
 
         isAway = team_away == "San Francisco Giants"
         isHome = team_home == "Arizona Diamondbacks"
@@ -19,14 +19,14 @@ class TestWebscrape(unittest.TestCase):
     def test_extractTeamID_extractsSameCapitalization(self):
         team_name = "Los Angeles Angels of Anaheim"
 
-        team_id = ws.extractTeamID(team_name)
+        team_id = wst.extractTeamID(team_name)
 
         self.assertEqual("LosAngelesAngelsofAnaheim", team_id)
 
     def test_extractTeamID_extractsOnlyAlphabets(self):
         team_name = "St. Louis Cardinals"
 
-        team_id = ws.extractTeamID(team_name)
+        team_id = wst.extractTeamID(team_name)
 
         self.assertEqual("StLouisCardinals", team_id)
 
