73a6e5493bf42c5579f796d2911c1f853905ae69
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-08-03 16:51:35 -0700

    initial structure for testing Player.py

---
+++ test/test_Player.py
@@ -0,0 +1,33 @@
+import unittest
+from code import Player
+
+class TestPlayer(unittest.TestCase):
+    def test_constructor_constructsHitter(self):
+        pass
+
+    def test_constructor_constructsPitcher(self):
+        pass
+
+    def test_equalOverload_returnsTrueIfSameHitter(self):
+        pass
+
+    def test_equalOverload_returnsFalseIfDifferentHitter(self):
+        pass
+
+    def test_equalOverload_returnsTrueIfSamePitcher(self):
+        pass
+
+    def test_equalOverload_returnsFalseIfDifferentPitcher(self):
+        pass
+
+    def test_update_updatesHitter(self):
+        pass
+
+    def test_update_updatesPitcher(self):
+        pass
+
+    def test_deepcopy_copiesHitter(self):
+        pass
+
+    def test_deepcopy_copiesPitcher(self):
+        pass
