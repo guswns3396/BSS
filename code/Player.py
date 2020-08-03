991f1f60412a7cdf161d1e0916f1e55a3167186b
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-08-03 12:34:16 -0700

    == overloading for Hitter & Pitcher

--- code/Player.py
+++ code/Player.py
@@ -12,6 +12,19 @@ class Hitter(Player):
         self.R = r
         self.L = l
 
+    def __eq__(self, other):
+        if not isinstance(other, Hitter):
+            raise NotImplemented
+        eqPA = self.PA  == other.PA
+        eqH = self.H == other.H
+        eqSO = self.SO == other.SO
+        eqR = self.R == other.R
+        eqL = self.L == other.L
+        if eqPA and eqH and eqSO and eqR and eqL:
+            return True
+        else:
+            return False
+
     def updateStats(self, data):
         self.PA += data['PA']
         self.H += data['H']
@@ -27,6 +40,21 @@ class Pitcher(Player):
         self.BF = bf
         self.R = r
         self.L = l
+
+    def __eq__(self, other):
+        if not isinstance(other, Pitcher):
+            raise NotImplemented
+        eqSHO = self.SHO  == other.SHO
+        eqIP = self.IP == other.IP
+        eqH = self.H == other.H
+        eqSO = self.SO == other.SO
+        eqBF = self.BF == other.BF
+        eqR = self.R == other.R
+        eqL = self.L == other.L
+        if eqSHO and eqIP and eqH and eqSO and eqBF and eqR and eqL:
+            return True
+        else:
+            return False
 
     def updateStats(self, data):
         self.SHO += data['SHO']
