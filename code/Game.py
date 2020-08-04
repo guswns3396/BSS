4f9fb3c53b27a1557612e7179ea837b6a4057c2f
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-08-04 13:17:51 -0700

    added constant for max number of hitters & pitchers; code for __str__

--- code/Game.py
+++ code/Game.py
@@ -1,5 +1,8 @@
 import code.Player as Player
 
+MAX_HITTERS = 26
+MAX_PITCHERS = 13
+
 class Game:
     def __init__(self, id, hitters, pitchers, outcome):
         """
@@ -22,3 +25,42 @@ class Game:
         :return: X, y
         """
         pass
+
+    def __str__(self):
+        row = []
+        row.append(self.id)
+        i = 0
+        for hitter in self.hitters:
+            row.append(hitter.PA)
+            row.append(hitter.H)
+            row.append(hitter.SO)
+            row.append(hitter.R)
+            row.append(hitter.L)
+            i += 1
+        while i < MAX_HITTERS:
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            i += 1
+        i = 0
+        for pitcher in self.pitchers:
+            row.append(pitcher.SHO)
+            row.append(pitcher.IP)
+            row.append(pitcher.H)
+            row.append(pitcher.SO)
+            row.append(pitcher.BF)
+            row.append(pitcher.R)
+            row.append(pitcher.L)
+            i += 1
+        while i < MAX_PITCHERS:
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            row.append(0)
+            i += 1
+        return ','.join(row)
