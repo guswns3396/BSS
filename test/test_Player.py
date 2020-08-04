402243186a21e57b62b687e13cca909e160a0c4a
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-08-03 17:06:09 -0700

    tests for Player.py

--- test/test_Player.py
+++ test/test_Player.py
@@ -1,33 +1,95 @@
 import unittest
+import copy
 from code import Player
 
 class TestPlayer(unittest.TestCase):
     def test_constructor_constructsHitter(self):
-        pass
+        player = Player.Hitter('Test Name','Test End',0,0,0,0,0)
+
+        self.assertIsInstance(player, Player.Hitter)
+
+    def test_constructor_constructsCorrectHitter(self):
+        player = Player.Hitter('Test Name', 'Test End', 0, 0, 0, 0, 0)
+
+        self.assertEqual(player.name, 'Test Name')
 
     def test_constructor_constructsPitcher(self):
-        pass
+        player = Player.Pitcher('Test Name','Test End',0,0,0,0,0,0,0)
+
+        self.assertIsInstance(player, Player.Pitcher)
+
+    def test_constructor_constructsCorrectPitcher(self):
+        player = Player.Pitcher('Test Name', 'Test End', 0, 0, 0, 0, 0, 0, 0)
+
+        self.assertEqual(player.name, 'Test Name')
 
     def test_equalOverload_returnsTrueIfSameHitter(self):
-        pass
+        p1 = Player.Hitter('1','1',0,0,0,0,0)
+        p2 = Player.Hitter('1','1',0,0,0,0,0)
+
+        isEqual = p1 == p2
+
+        self.assertTrue(isEqual)
 
     def test_equalOverload_returnsFalseIfDifferentHitter(self):
-        pass
+        p1 = Player.Hitter('1', '1', 0, 0, 0, 0, 0)
+        p2 = Player.Hitter('2', '2', 0, 0, 0, 0, 0)
+
+        isNotEqual = p1 != p2
+
+        self.assertTrue(isNotEqual)
 
     def test_equalOverload_returnsTrueIfSamePitcher(self):
-        pass
+        p1 = Player.Pitcher('1', '1', 0, 0, 0, 0, 0, 0, 0)
+        p2 = Player.Pitcher('1', '1', 0, 0, 0, 0, 0, 0, 0)
+
+        isEqual = p1 == p2
+
+        self.assertTrue(isEqual)
 
     def test_equalOverload_returnsFalseIfDifferentPitcher(self):
-        pass
+        p1 = Player.Pitcher('1', '1', 0, 0, 0, 0, 0, 0, 0)
+        p2 = Player.Pitcher('2', '2', 0, 0, 0, 0, 0, 0, 0)
+
+        isNotEqual = p1 != p2
+
+        self.assertTrue(isNotEqual)
 
     def test_update_updatesHitter(self):
-        pass
+        data = {'PA': 1, 'H': 1, 'SO': 1}
+        player = Player.Hitter('', '', 0, 0, 0, 0, 0)
+
+        player.updateStats(data)
+
+        expected = Player.Hitter('','',1,1,1,0,0)
+        self.assertEqual(expected, player)
 
     def test_update_updatesPitcher(self):
-        pass
+        data = {'SHO': 1, 'IP': 1, 'H': 1, 'SO': 1, 'BF': 1}
+        player = Player.Pitcher('', '', 0, 0, 0, 0, 0, 0, 0)
+
+        player.updateStats(data)
+
+        expected = Player.Pitcher('', '', 1, 1, 1, 1, 1, 0, 0)
+        self.assertEqual(expected, player)
 
     def test_deepcopy_copiesHitter(self):
-        pass
+        hitters = []
+        hitter = Player.Hitter('','',0,0,0,0,0)
+
+        hitters.append(copy.deepcopy(hitter))
+
+        hitter.updateStats({'PA': 1, 'H': 1, 'SO': 1})
+        self.assertNotEqual(hitters[0], hitter)
 
     def test_deepcopy_copiesPitcher(self):
-        pass
+        pitchers = []
+        pitcher = Player.Pitcher('', '', 0, 0, 0, 0, 0, 0, 0)
+
+        pitchers.append(copy.deepcopy(pitcher))
+
+        pitcher.updateStats({'SHO': 1, 'IP': 1, 'H': 1, 'SO': 1, 'BF': 1})
+        self.assertNotEqual(pitchers[0], pitcher)
+
+if __name__ == '__main__':
+    unittest.main()
