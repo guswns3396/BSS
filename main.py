f7747454fe4c03be3b0e6414428a4bd7839731d9
Hyun-Joon Yang <yhyunjoon@gmail.com>
2020-08-05 12:36:02 -0700

    modified main to take in command line arguments

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 import argparse
-import csv
+import code.webscrape as ws
 
 def main(args):
-    path = args["path"]
-    with open(path) as fh:
-        rd = csv.DictReader(fh, delimiter=',')
-        for row in rd:
-            print(row)
+    # if gather data chosen
+    if args['years']:
+        pass
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path",
-                        action="store",
-                        help="file path to csv of players",
-                        dest="path")
+    parser = argparse.ArgumentParser(description='Run BSS')
+    group = parser.add_mutually_exclusive_group(required=True)
+    # gather data
+    group.add_argument('-g', '--gather',
+                       action='store',
+                       type=int,
+                       nargs=2,
+                       metavar='YEAR',
+                       dest='years',
+                       help='Gather data from start_year to end_year')
+    parser.add_argument('-o', '--output',
+                        action='store',
+                        metavar='FILENAME',
+                        default='games.csv',
+                        dest='filename',
+                        help='Name of file to output after gathering data')
+
+    group.add_argument('-t', '--train', help='Train model')
+    group.add_argument('-i', '--inference', help='Run inference')
+
     args = vars(parser.parse_args())
-    main(args)
